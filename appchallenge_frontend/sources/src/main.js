571642007f7e6b6e81c4034dae616c3408fc9f42
pedro <phsouzaa2@gmail.com>
2022-05-24 14:28:48 -0300

    Adicionando dependências Vuetify e Router ao projeto e criando página Home para teste inicial

--- appchallenge_frontend/sources/src/main.js
+++ appchallenge_frontend/sources/src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import Home from '@/pages/Home'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = 
+[
+  {
+    path:'/',
+    component:Home,
+    meta:{}
+  },
+  {
+    path:'/Home',
+    component:Home,
+    meta:{}
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+});
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ appchallenge_frontend/sources/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="main">
+        <v-banner app color="green" dark>
+            <div class="d-flex align-center">
+                <v-title class="text-h1">
+                    <v-icon large left>
+                        mdi-wifi
+                    </v-icon>
+                    FindConnection
+                </v-title>
+            </div>
+        </v-banner>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home-page'
+}
+</script>
