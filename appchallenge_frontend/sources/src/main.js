a78703554780e86b21b089cd0907b518804e358a
pedro <phsouzaa2@gmail.com>
2022-05-25 16:31:22 -0300

    Adicionando integração com API do App Challenge

--- appchallenge_frontend/sources/src/main.js
+++ appchallenge_frontend/sources/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGeolocationApi from 'vue-geolocation-api'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import Home from '@/pages/Home'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(VueGeolocationApi/*, { ...options } */)
 
 const routes = 
 [
--- appchallenge_frontend/sources/src/pages/Home.vue
+++ appchallenge_frontend/sources/src/pages/Home.vue
@@ -1,20 +1,57 @@
 <template>
-    <div id="main">
-        <v-banner app color="green" dark>
-            <div class="d-flex align-center">
-                <v-title class="text-h1">
-                    <v-icon large left>
-                        mdi-wifi
-                    </v-icon>
-                    FindConnection
-                </v-title>
-            </div>
-        </v-banner>
-    </div>
+  <div id="main">
+    <v-banner app color="green" dark>
+      <div class="d-flex align-center">
+        <v-title class="text-h1">
+          <v-icon large left>
+            mdi-wifi
+          </v-icon>
+          FindConnection
+        </v-title>
+      </div>
+    </v-banner>
+    <v-card-text>
+      <v-autocomplete v-model="model" :items="item" :loading="isLoading" :search-input.sync="search"
+        color="white" hide-no-data hide-selected item-text="Description" item-value="API" label="Public APIs"
+        placeholder="Start typing to Search" prepend-icon="mdi-database-search" return-object></v-autocomplete>
+    </v-card-text>
+
+    <li v-for="item in apiInformation" :key="item.isp">
+      {{ item.isp }}
+    </li>
+
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'home-page'
+  name: 'home-page',
+
+  data() {
+    return {
+      apiInformation: [],
+    };
+  },
+
+
+  mounted() {
+    axios
+      .get("https://app-challenge-api.herokuapp.com/plans").then((response) => {
+        this.apiInformation = response.data
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  computed: {
+    ispNames: function () {
+      return this.apiInformation.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  }
 }
 </script>
