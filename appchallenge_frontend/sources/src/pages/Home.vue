2bc12fbe441a7dbd376697f400a0486642b0d0b3
pedro <phsouzaa2@gmail.com>
2022-05-25 23:13:53 -0300

    Criando paineis e consumindo API de provedores na tela principal

--- appchallenge_frontend/sources/src/pages/Home.vue
+++ appchallenge_frontend/sources/src/pages/Home.vue
@@ -1,33 +1,99 @@
 <template>
   <div id="main">
-    <v-banner app color="green" dark>
+    <v-banner color="green" dark>
       <div class="d-flex align-center">
-        <v-title class="text-h1">
-          <v-icon large left>
-            mdi-wifi
-          </v-icon>
+        <v-title class="text-xs-h5 text-sm-h3 text-md-h1">
+          <v-icon large left> mdi-wifi </v-icon>
           FindConnection
         </v-title>
       </div>
     </v-banner>
-    <v-card-text>
-      <v-autocomplete v-model="model" :items="item" :loading="isLoading" :search-input.sync="search"
-        color="white" hide-no-data hide-selected item-text="Description" item-value="API" label="Public APIs"
-        placeholder="Start typing to Search" prepend-icon="mdi-database-search" return-object></v-autocomplete>
-    </v-card-text>
 
-    <li v-for="item in apiInformation" :key="item.isp">
-      {{ item.isp }}
-    </li>
+    <div class="ma-12 elevation-24">
+      <v-expansion-panels>
+        <v-expansion-panel
+          v-for="(item, company) in apiInformation"
+          :key="item"
+        >
+          <v-expansion-panel-header> {{ company }} </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4 lg3 v-for="plan in item" :key="plan">
+                <v-card class="ma-2 elevation-24">
+                  <template slot="progress">
+                    <v-progress-linear
+                      color="deep-purple"
+                      height="10"
+                      indeterminate
+                    ></v-progress-linear>
+                  </template>
 
+                  <v-card-title
+                    >Plano: {{ plan.download_speed }} MBs</v-card-title
+                  >
+
+                  <v-card-text>
+                    <v-row align="center" class="mx-0">
+                      <v-rating
+                        :value="4.5"
+                        color="amber"
+                        dense
+                        half-increments
+                        readonly
+                        size="14"
+                      ></v-rating>
+
+                      <div class="grey--text ms-4">4.5 (413)</div>
+                    </v-row>
+
+                    <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+                    <div>
+                      Small plates, salads & sandwiches - an intimate setting
+                      with 12 indoor seats plus patio seating.
+                    </div>
+                  </v-card-text>
+
+                  <v-divider class="mx-4"></v-divider>
+
+                  <v-card-title>Tonight's availability</v-card-title>
+
+                  <v-card-text>
+                    <v-chip-group
+                      v-model="selection"
+                      active-class="deep-purple accent-4 white--text"
+                      column
+                    >
+                      <v-chip>5:30PM</v-chip>
+
+                      <v-chip>7:30PM</v-chip>
+
+                      <v-chip>8:00PM</v-chip>
+
+                      <v-chip>9:00PM</v-chip>
+                    </v-chip-group>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-btn color="deep-purple lighten-2" text @click="reserve">
+                      Reserve
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'home-page',
+  name: "home-page",
 
   data() {
     return {
@@ -35,11 +101,25 @@ export default {
     };
   },
 
-
   mounted() {
     axios
-      .get("https://app-challenge-api.herokuapp.com/plans").then((response) => {
-        this.apiInformation = response.data
+      .get("https://app-challenge-api.herokuapp.com/plans")
+      .then((response) => {
+        if (response.data != null)
+          this.apiInformation = response.data.reduce((group, row) => {
+            const { isp } = row;
+            group[isp] = group[isp] ?? [];
+            group[isp].push(row);
+            return group;
+          }, {});
+
+        // response.data.forEach((row) => {
+        //   if (!this.apiInformation[row.isp])
+        //     this.apiInformation[row.isp] = []
+        //     this.apiInformation[row.isp].push(row)
+        //})
+        console.log(this.apiInformation);
+        //this.apiInformation = response.data
       })
       .catch((error) => {
         console.log(error);
@@ -47,11 +127,11 @@ export default {
   },
 
   computed: {
-    ispNames: function () {
-      return this.apiInformation.filter(function (number) {
-        return number % 2 === 0
-      })
-    }
-  }
-}
+    planSelected() {
+      return this.apiInformation.filter((plan) =>
+        this.apiInformation.data.includes(plan.id)
+      );
+    },
+  },
+};
 </script>
